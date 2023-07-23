7a2559c6f3f2900f5c999722bdd7faf171e29f1a
MarkParagas <mark.paragas@student.ehb.be>
2023-07-23 16:45:52 +0200

    Update unit.test
    - Error fixed, is Pass in 'npm test'
    
    - 'It' is check as one, need two separate to check two testing.
    
    - test2 is toBefalsy because is false

--- api/_test/unit.test.js
+++ api/_test/unit.test.js
@@ -1,5 +1,6 @@
 const submit = require("./../src/submit");
 
+// Test 1 is true
 const test = {
   date: "2023-07-12",
   workout_name: "Back",
@@ -11,30 +12,30 @@ const test = {
       sets: 5,
       reps: 12,
     },
-  ]
+  ],
 };
 
 // Test 2 is false
 const test2 = {
-    date: "2023-07-12",
-    workout_name: "Abs",
-    duration: 60,
-    exercises: [
-      {
-        _id: "64b85ab8a0a33a77d",
-        exercise_name: "Chin-ups",
-        sets: 5,
-        reps: -10,
-      },
-    ]
-  };
+  date: "2023-07-12",
+  workout_name: "Abs",
+  duration: 60,
+  exercises: [
+    {
+      _id: "64b85ab8a0a33a77d",
+      exercise_name: "Chin-ups",
+      sets: 5,
+      reps: -10,
+    },
+  ],
+};
 
 describe("Testing POSTS", () => {
-  it("check Workout Submit", () => {
+  it("check Workout Submit with Valid Data", () => {
     expect(submit.checkSubmitPOST(test)).toBeTruthy(); // True and successful POST
-    expect(submit.checkSubmitPOST({})).toBeFalsy();
+  });
 
-    expect(submit.checkSubmitPOST(test2)).toBeTruthy(); // False and successful POST
-    expect(submit.checkSubmitPOST({})).toBeFalsy(); 
+  it("check Workout Submit with Invalid Data", () => {
+    expect(submit.checkSubmitPOST(test2)).toBeFalsy(); // False and get error instead
   });
 });
