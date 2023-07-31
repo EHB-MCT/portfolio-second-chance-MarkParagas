5ae708f30876b0d2f9204fe5fa4ec9d7d9828d13
MarkParagas <mark.paragas@student.ehb.be>
2023-07-31 23:36:46 +0200

    Update models & Integration
    - Further coding the integration test

--- api/model/model.js
+++ api/model/model.js
@@ -1,16 +1,62 @@
 const mongoose = require("mongoose");
 
 const workoutSchema = new mongoose.Schema({
-  date: String,
-  workout_name:  String,
-  duration: Number,
+  date: {
+    type: String,
+    required: true,
+  },
+  workout_name: {
+    type: String,
+    enum: [
+      "Chest",
+      "Biceps",
+      "Triceps",
+      "Shoulders",
+      "Abs",
+      "Back",
+      "Legs",
+      "Forearms",
+    ],
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value >= 0;
+      },
+      message: "Duration cannot be a negative number",
+    },
+  },
   exercises: [{ type: mongoose.Schema.Types.ObjectId, ref: "Exercise" }],
 });
 
 const exerciseSchema = new mongoose.Schema({
-  exercise_name: String,
-  sets: Number,
-  reps: Number,
+  exercise_name: {
+    type: String,
+    required: true,
+  },
+  sets: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value >= 0;
+      },
+      message: "Sets cannot be a negative number",
+    },
+  },
+  reps: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value >= 0;
+      },
+      message: "Reps cannot be a negative number",
+    },
+  },
 });
 
 module.exports = {
