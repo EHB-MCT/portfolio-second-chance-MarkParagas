820dd0df628bff7a624ef01154bfefaefe567c37
MarkParagas <mark.paragas@student.ehb.be>
2023-08-01 03:25:28 +0200

    Update Unit test
    - Update submit.js that separate the workout and exercise posts
    
    - Unit test total: 20 added

--- api/_test/unit.test.js
+++ api/_test/unit.test.js
@@ -1,7 +1,71 @@
 const submit = require("./../src/submit");
 
+// Test 1 is true | Everything inputs is filled
+const exerciseTest = {
+      exercise_name: "Deadlift",
+      sets: 5,
+      reps: 12,
+};
+
+// Test 2 is false | exercise_name cannot be empty
+const exerciseTest2 = {
+  exercise_name: "",
+  sets: 5,
+  reps: 12,
+};
+
+// Test 3 is false | sets cannot be empty
+const exerciseTest3 = {
+  exercise_name: "Deadlift",
+  sets: "",
+  reps: 12,
+};
+
+// Test 4 is false | reps cannot be empty
+const exerciseTest4 = {
+  exercise_name: "Deadlift",
+  sets: 5,
+  reps: "",
+};
+
+// Test 5 is false | Inputs cannot be empty
+const exerciseTest5 = {
+  exercise_name: "",
+  sets: "",
+  reps: "",
+};
+
+// Test 6 is false | Sets input is negative
+const exerciseTest6 = {
+  exercise_name: "Deadlift",
+  sets: -5,
+  reps: 12,
+};
+
+// Test 7 is false | Reps input is negative
+const exerciseTest7 = {
+  exercise_name: "Deadlift",
+  sets: 5,
+  reps: -12,
+};
+
+// Test 8 is false | Sets value is zero
+const exerciseTest8 = {
+  exercise_name: "Deadlift",
+  sets: 0,
+  reps: 12,
+};
+
+// Test 9 is false | Reps value is zero
+const exerciseTest9 = {
+  exercise_name: "Deadlift",
+  sets: 5,
+  reps: 0,
+};
+
+
 // Test 1 is true | Everything inputs is filled and exercise id exist
-const test = {
+const workoutTest = {
   date: "2023-07-12",
   workout_name: "Back",
   duration: 20,
@@ -16,7 +80,7 @@ const test = {
 };
 
 // Test 2 is false | _id does not exist
-const test2 = {
+const workoutTest2 = {
   date: "2023-07-12",
   workout_name: "Abs",
   duration: 60,
@@ -31,7 +95,7 @@ const test2 = {
 };
 
 // Test 3 is false | Missing _id field for an exercise
-const test3 = {
+const workoutTest3 = {
   date: "2023-07-12",
   workout_name: "Chest",
   duration: 30,
@@ -45,7 +109,7 @@ const test3 = {
 };
 
 // Test 4 is false | workout_name cannot be empty
-const test4 = {
+const workoutTest4 = {
   date: "2023-07-12",
   workout_name: "",
   duration: 45,
@@ -60,7 +124,7 @@ const test4 = {
 };
 
 // Test 5 is false | date cannot be empty
-const test5 = {
+const workoutTest5 = {
   date: "",
   workout_name: "Legs",
   duration: 40,
@@ -76,7 +140,7 @@ const test5 = {
 
 
 // Test 6 is true | Exercise sets with negative value
-const test6 = {
+const workoutTest6 = {
   date: "2023-07-12",
   workout_name: "Back",
   duration: 20,
@@ -91,7 +155,7 @@ const test6 = {
 };
 
 // Test 7 is true | Exercise reps with negative value
-const test7 = {
+const workoutTest7 = {
   date: "2023-07-12",
   workout_name: "Back",
   duration: 20,
@@ -106,7 +170,7 @@ const test7 = {
 };
 
 // Test 8 is false | Exercise sets with zero value
-const test8 = {
+const workoutTest8 = {
   date: "2023-07-12",
   workout_name: "Back",
   duration: 20,
@@ -121,7 +185,7 @@ const test8 = {
 };
 
 // Test 9 is false | Exercise reps with zero value
-const test9 = {
+const workoutTest9 = {
   date: "2023-07-12",
   workout_name: "Back",
   duration: 20,
@@ -136,7 +200,7 @@ const test9 = {
 };
 
 // Test 10 is false | Workout duration with zero value
-const test10 = {
+const workoutTest10 = {
   date: "2023-07-12",
   workout_name: "Back",
   duration: 0,
@@ -150,46 +214,101 @@ const test10 = {
   ],
 };
 
+// Test 11 is false | Exercise are Empty inputs
+const workoutTest11 = {
+  date: "2023-07-12",
+  workout_name: "Back",
+  duration: 20,
+  exercises: [
+    {
+      _id: "",
+      exercise_name: "",
+      sets: "",
+      reps: "",
+    },
+  ],
+};
+
 
 describe("Testing POST of Unit Test", () => {
   it("check Workout Submit with Valid Data", () => {
-    expect(submit.checkSubmitPOST(test)).toBeTruthy(); // True due Everything inputs is filled and exercise id exist
+    expect(submit.checkWorkoutPOST(workoutTest)).toBeTruthy(); // True due Everything inputs is filled and exercise id exist
   });
 
   it("check Workout Submit with Invalid Data", () => {
-    expect(submit.checkSubmitPOST(test2)).toBeFalsy(); // False due _id does not exist
+    expect(submit.checkWorkoutPOST(workoutTest2)).toBeFalsy(); // False due _id does not exist
   });
 
   it("check Workout Submit with Missing Exercise _id", () => {
-    expect(submit.checkSubmitPOST(test3)).toBeFalsy(); // False due to missing _id field 
+    expect(submit.checkWorkoutPOST(workoutTest3)).toBeFalsy(); // False due to missing _id field 
   });
 
   it("check Workout Submit with Empty workout_name", () => {
-    expect(submit.checkSubmitPOST(test4)).toBeFalsy(); // False due to empty workout_name
+    expect(submit.checkWorkoutPOST(workoutTest4)).toBeFalsy(); // False due to empty workout_name
   });
 
   it("check Workout Submit with Empty date", () => {
-    expect(submit.checkSubmitPOST(test5)).toBeFalsy(); // False due to empty date
+    expect(submit.checkWorkoutPOST(workoutTest5)).toBeFalsy(); // False due to empty date
   });
 
   it("check Workout Submit with exercise sets with negative value", () => {
-    expect(submit.checkSubmitPOST(test6)).toBeFalsy(); // False due to exercise sets with negative value
+    expect(submit.checkWorkoutPOST(workoutTest6)).toBeFalsy(); // False due to exercise sets with negative value
   });
 
   it("check Workout Submit with exercise reps with negative value", () => {
-    expect(submit.checkSubmitPOST(test7)).toBeFalsy(); // False due to exercise reps with negative value
+    expect(submit.checkWorkoutPOST(workoutTest7)).toBeFalsy(); // False due to exercise reps with negative value
   });
 
   it("check Workout Submit with exercise sets with 0 value", () => {
-    expect(submit.checkSubmitPOST(test8)).toBeFalsy(); // False, sets value is zero
+    expect(submit.checkWorkoutPOST(workoutTest8)).toBeFalsy(); // False, sets value is zero
   });
 
   it("check Workout Submit with exercise reps with 0 value", () => {
-    expect(submit.checkSubmitPOST(test9)).toBeFalsy(); // False, reps value is zero
+    expect(submit.checkWorkoutPOST(workoutTest9)).toBeFalsy(); // False, reps value is zero
   });
 
   it("check Workout Submit with workout duration with 0 value", () => {
-    expect(submit.checkSubmitPOST(test10)).toBeFalsy(); // False, duration value is zero
+    expect(submit.checkWorkoutPOST(workoutTest10)).toBeFalsy(); // False, duration value is zero
+  });
+
+  it("check Workout, Submit with exercise empty inputs", () => {
+    expect(submit.checkWorkoutPOST(workoutTest11)).toBeFalsy(); // False | Exercise inputs are Empty inputs
+  });
+
+  it("check Exercise is inputs are all filled", () => {
+    expect(submit.checkExercisePOST(exerciseTest)).toBeTruthy(); // True due every inputs is filled
+  });
+
+  it("check Exercise with exercise_name are empty", () => {
+    expect(submit.checkExercisePOST(exerciseTest2)).toBeFalsy(); // False due exercise_name are empty
+  });
+
+  it("check Exercise with Sets are empty", () => {
+    expect(submit.checkExercisePOST(exerciseTest3)).toBeFalsy(); // False due Sets are empty
+  });
+
+  it("check Exercise with Reps are empty", () => {
+    expect(submit.checkExercisePOST(exerciseTest4)).toBeFalsy(); // False due Reps are empty
+  });
+
+  it("check Exercise with Inputs are empty ", () => {
+    expect(submit.checkExercisePOST(exerciseTest5)).toBeFalsy(); // False due Inputs cannot be empty
+  });
+
+  it("check Exercise with Sets input are negative ", () => {
+    expect(submit.checkExercisePOST(exerciseTest6)).toBeFalsy(); // False due Sets input is negative
+  });
+
+  it("check Exercise with Reps input are negative ", () => {
+    expect(submit.checkExercisePOST(exerciseTest7)).toBeFalsy(); // False due Reps input is negative
+  });
+
+  it("check Exercise with Sets value zero", () => {
+    expect(submit.checkExercisePOST(exerciseTest8)).toBeFalsy(); // False due Sets value is zero
+  });
+
+  it("check Exercise with Reps value zero ", () => {
+    expect(submit.checkExercisePOST(exerciseTest9)).toBeFalsy(); // False due Reps value is zero
   });
 
 });
