34871920d2ffcb80b3dc5c0e573942ac4f1b1197
MarkParagas <mark.paragas@student.ehb.be>
2023-08-01 03:05:31 +0200

    Update models
    - Update Model.js
    - Update submit.js base of model (for testing)

--- api/src/submit.js
+++ api/src/submit.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 function checkSubmitPOST(data) {
-  const { date, workout_name, exercises } = data;
+  const { date, workout_name, duration, exercises } = data;
 
   let listOfWorkouts = [
     "Chest",
@@ -16,9 +16,11 @@ function checkSubmitPOST(data) {
     "Forearms",
   ];
 
+  // Workout POST validation
   if (
     date &&
     workout_name &&
+    duration > 0 &&
     exercises &&
     listOfWorkouts.includes(workout_name) &&
     exercises.length > 0 &&
@@ -26,15 +28,27 @@ function checkSubmitPOST(data) {
       (exercise) =>
         exercise._id &&
         exercise._id.length === 24 && // MongoDB is 24 char / 12 bytes in length (checker) &&
-        exercise.exercise_name &&
-        Number.isInteger(exercise.sets) && exercise.sets >= 0 &&
-        Number.isInteger(exercise.reps) && exercise.reps >= 0
+        checkExercisePOST(exercise) // Validate individual exercises
     )
   ) {
-    return true; // Data submit & Successful
+    return true;
   } else {
-    return false; // Get data errors
+    return false; 
   }
 }
 
-module.exports = { checkSubmitPOST };
+function checkExercisePOST(exercise) {
+  if (
+    exercise.exercise_name &&
+    Number.isInteger(exercise.sets) &&
+    exercise.sets > 1 &&
+    Number.isInteger(exercise.reps) &&
+    exercise.reps > 1
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = { checkSubmitPOST, checkExercisePOST };
